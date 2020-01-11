17ceb6a37186ce7ff1c890e483372097a5063a18
TiagoGB <tiago.buttenbender@gmail.com>
2020-01-11 17:58:43 -0300

    Create Sub - Habilita Otimização.vb

---
+++ Sub - Habilita Otimização.vb
@@ -0,0 +1,11 @@
+Sub sEnableOptimization()
+    
+    With Application
+        
+        .ScreenUpdating = True
+        .Calculation = xlCalculationAutomatic
+        .DisplayAlerts = True
+        
+    End With
+
+End Sub
