3478a0f2a5fc5747385795459bcd7da13d50ea56
TiagoGB <tiago.buttenbender@gmail.com>
2020-01-11 17:29:04 -0300

    Add files via upload

---
+++ Function - Ultima linha com dados.vb
@@ -0,0 +1,19 @@
+Function fLastRowData( _
+    ByRef pWorkbookName, _
+    ByVal pSheetName As String, _
+    ByVal pCol As String _
+    )
+
+    Dim vR As Long
+    Dim vLr As Long
+    vR = 0
+    vLr = 1
+    Do
+        vR = vLr
+            vLr = Workbooks(pWorkbookName).Worksheets(pSheetName).Range(pCol & vR).End(xlDown).Row
+    Loop While vLr <> vR
+    vLr = Workbooks(pWorkbookName).Worksheets(pSheetName).Range(pCol & vR).End(xlUp).Row
+    
+    fLastRowData = vLr
+    
+End Function
