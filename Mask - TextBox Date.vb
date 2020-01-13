a396ea0a1dc6e6157801734287b8f954bf623e92
TiagoGB <tiago.buttenbender@gmail.com>
2020-01-12 22:58:41 -0300

    Delete Mask - TextBox Date.vb

--- Mask - TextBox Date.vb
+++
@@ -1,9 +0,0 @@
-Private Sub txtData_KeyPress(ByVal KeyAscii As MSForms.ReturnInteger)
-    If Not IsNumeric(Chr(KeyAscii.Value)) Or Len(txtData.Text) >= 10 Then
-        KeyAscii.Value = 0
-    Else
-        If Len(txtData.Text) = 2 Or Len(txtData.Text) = 5 Then
-            txtData.Text = txtData.Text & "/"
-        End If
-    End If
-End Sub
