fe0d9304d969974f9e95c62c12023d88a44ee4ce
TiagoGB <tiago.buttenbender@gmail.com>
2020-01-12 22:59:05 -0300

    Delete Sub - Protect Sheet.vb

--- Sub - Protect Sheet.vb
+++
@@ -1,5 +0,0 @@
-Sub sProtect(Byval pWorkbookName As String, ByVal pSheetName As String)
-    With Workbooks(pWorkbookName).Worksheets(pSheetName)
-    .Protect Password:=gPass, DrawingObjects:=True, Contents:=True, Scenarios:=True, AllowSorting:=True, AllowFiltering:=True
-    End With
-End Sub
